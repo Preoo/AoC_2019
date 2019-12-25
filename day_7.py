a23c16363c03037074dc620b94c58c1a5a6107bb
Preoo <Preoo@users.noreply.github.com>
2019-12-25 02:54:03 +0200

    runs day 7 part 2 examples correctly.

--- day_7.py
+++ day_7.py
@@ -1,20 +1,11 @@
 import day_5 as opcode_machine
 from itertools import permutations
 
-class Amplifier:
-
-    def __init__(self, machine, phase):
-        self.machine = machine
-        self.phase = phase
-        self.input = 0
-        self.output = 0
-        self.outputs_to = None
-
 
 if __name__ == "__main__":
     program = [3,8,1001,8,10,8,105,1,0,0,21,46,67,88,101,126,207,288,369,450,99999,3,9,1001,9,5,9,1002,9,5,9,1001,9,5,9,102,3,9,9,101,2,9,9,4,9,99,3,9,102,4,9,9,101,5,9,9,102,5,9,9,101,3,9,9,4,9,99,3,9,1001,9,3,9,102,2,9,9,1001,9,5,9,102,4,9,9,4,9,99,3,9,102,3,9,9,1001,9,4,9,4,9,99,3,9,102,3,9,9,1001,9,3,9,1002,9,2,9,101,4,9,9,102,3,9,9,4,9,99,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,99,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,99,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,99]
 
-    #get all possible integer permutations of set {0, .. , 4}
+    #Part 1
     phase_settings = set(permutations(range(5)))
 
     thruster_signals = set()
@@ -34,22 +25,28 @@ if __name__ == "__main__":
 
     print(f'Max thruster signal is {max(thruster_signals)}')
 
-
+    #Part 2
     phase_settings = set(permutations(range(5, 10)))
-    
-    test_phase = (9,8,7,6,5)
+
+    from day_5 import IntCodeMachine as m
+
     test_prog = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
+    test_phase = [9,8,7,6,5]
 
-    input_signal = [0]
-    outputs = []
-    for i, amplifier in enumerate(['A', 'B', 'C', 'D', 'E']):
-        
-        opcode_machine.run(test_prog.copy(), inputs=[test_phase[i]] + input_signal, outputs=outputs)
+    machines = [m(test_prog.copy()) for _ in range(5)]
+    for i in range(5):
+        machines[i].set_input_source(machines[i-1].io_o)
+    
 
-        print(f'Amplifier {amplifier} return signal {outputs} for input {input_signal} and phase {test_phase[i]}.')
+    for m, p in zip(machines, test_phase):
+        m.feed_inputs([p])
 
-        input_signal.extend(outputs.copy())
-        
+    machines[0].feed_inputs([0])
 
-    print(f'New part 2 output: {outputs}')
-
+    while not machines[1].halted:
+        for i, m in enumerate(machines):
+            for _ in m.run():
+                pass
+            
+        print(machines[-1].io_o)
+            
