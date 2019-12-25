7e40098df7bf7fa5c4a3827b0f5871ed6aaf7bdc
Preoo <Preoo@users.noreply.github.com>
2019-12-25 13:19:23 +0200

    solves day 7 part 2.

--- day_7.py
+++ day_7.py
@@ -30,23 +30,29 @@ if __name__ == "__main__":
 
     from day_5 import IntCodeMachine as m
 
-    test_prog = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
-    test_phase = [9,8,7,6,5]
+    test_prog = [3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10]
+    test_phase = [9,7,8,5,6]
 
-    machines = [m(test_prog.copy()) for _ in range(5)]
-    for i in range(5):
-        machines[i].set_input_source(machines[i-1].io_o)
-    
+    thruster_signals.clear()
 
-    for m, p in zip(machines, test_phase):
-        m.feed_inputs([p])
+    for phase_setting in phase_settings:
 
-    machines[0].feed_inputs([0])
+        machines = [m(program.copy()) for _ in range(5)]
+        for i in range(5):
+            machines[i].set_input_source(machines[i-1].io_o)
+        
 
-    while not machines[1].halted:
-        for i, m in enumerate(machines):
-            for _ in m.run():
-                pass
-            
+        for mc, p in zip(machines, phase_setting):
+            mc.feed_inputs([p])
+
+        machines[0].feed_inputs([0])
+
+        while not machines[-1].halted:
+            for i, ma in enumerate(machines):
+                for _ in ma.run():
+                    pass
+                
         print(machines[-1].io_o)
-            
+        thruster_signals.add(machines[-1].io_o[0])
+        
+    print(f'Max thruster signal is {max(thruster_signals)}')        
